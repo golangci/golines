f41d6d9f7276f8a9f9796f3b30f2447dff177dc4
Fernandez Ludovic <ldez@users.noreply.github.com>
2025-09-18 20:45:55 +0200

    chore: refactor and add tests

--- shortener/internal/annotation/annotation_test.go
+++ shortener/internal/annotation/annotation_test.go
@@ -7,38 +7,157 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_strings(t *testing.T) {
+func TestCreate(t *testing.T) {
 	assert.Equal(t, "// __golines:shorten:5", Create(5))
-	assert.Equal(t, 5, Parse("// __golines:shorten:5"))
-	assert.Equal(t, -1, Parse("// __golines:shorten:not_a_number"))
-	assert.Equal(t, -1, Parse("// not an annotation"))
-	assert.True(t, Is("// __golines:shorten:5"))
-	assert.False(t, Is("// not an annotation"))
+}
+
+func TestIs(t *testing.T) {
+	testCases := []struct {
+		desc   string
+		line   string
+		assert assert.BoolAssertionFunc
+	}{
+		{
+			desc:   "annotation",
+			line:   "// __golines:shorten:5",
+			assert: assert.True,
+		},
+		{
+			desc:   "not an annotation",
+			line:   "// not an annotation",
+			assert: assert.False,
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.desc, func(t *testing.T) {
+			t.Parallel()
+
+			test.assert(t, Is(test.line))
+		})
+	}
 }
 
 func TestHas(t *testing.T) {
-	node1 := &dst.Ident{
-		Name: "x",
-		Decs: dst.IdentDecorations{
-			NodeDecs: dst.NodeDecs{
-				Start: []string{
-					"// not an annotation",
-					Create(55),
+	testCases := []struct {
+		desc   string
+		node   *dst.Ident
+		assert assert.BoolAssertionFunc
+	}{
+		{
+			desc: "",
+			node: &dst.Ident{
+				Name: "x",
+				Decs: dst.IdentDecorations{
+					NodeDecs: dst.NodeDecs{
+						Start: []string{
+							"// not an annotation",
+							Create(55),
+						},
+					},
 				},
 			},
+			assert: assert.True,
+		},
+		{
+			desc: "",
+			node: &dst.Ident{
+				Name: "x",
+				Decs: dst.IdentDecorations{
+					NodeDecs: dst.NodeDecs{
+						Start: []string{
+							"// not an annotation",
+						},
+					},
+				},
+			},
+			assert: assert.False,
 		},
 	}
-	assert.True(t, Has(node1))
 
-	node2 := &dst.Ident{
-		Name: "x",
-		Decs: dst.IdentDecorations{
-			NodeDecs: dst.NodeDecs{
-				Start: []string{
-					"// not an annotation",
+	for _, test := range testCases {
+		t.Run(test.desc, func(t *testing.T) {
+			t.Parallel()
+
+			test.assert(t, Has(test.node))
+		})
+	}
+}
+
+func TestHasTail(t *testing.T) {
+	testCases := []struct {
+		desc   string
+		node   *dst.Ident
+		assert assert.BoolAssertionFunc
+	}{
+		{
+			desc: "tail",
+			node: &dst.Ident{
+				Name: "x",
+				Decs: dst.IdentDecorations{
+					NodeDecs: dst.NodeDecs{
+						End: []string{
+							"// not an annotation",
+							Create(55),
+						},
+					},
 				},
 			},
+			assert: assert.True,
+		},
+		{
+			desc: "no tail",
+			node: &dst.Ident{
+				Name: "x",
+				Decs: dst.IdentDecorations{
+					NodeDecs: dst.NodeDecs{
+						End: []string{
+							"// not an annotation",
+						},
+					},
+				},
+			},
+			assert: assert.False,
 		},
 	}
-	assert.False(t, Has(node2))
+
+	for _, test := range testCases {
+		t.Run(test.desc, func(t *testing.T) {
+			t.Parallel()
+
+			test.assert(t, HasTail(test.node))
+		})
+	}
+}
+
+func TestParse(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		line     string
+		expected int
+	}{
+		{
+			desc:     "valid annotation",
+			line:     "// __golines:shorten:5",
+			expected: 5,
+		},
+		{
+			desc:     "not a number",
+			line:     "// __golines:shorten:not_a_number",
+			expected: -1,
+		},
+		{
+			desc:     "not an annotation",
+			line:     "// not an annotation",
+			expected: -1,
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.desc, func(t *testing.T) {
+			t.Parallel()
+
+			assert.Equal(t, test.expected, Parse(test.line))
+		})
+	}
 }
--- shortener/internal/graph/graph_test.go
+++ shortener/internal/graph/graph_test.go
@@ -2,7 +2,7 @@ package graph
 
 import (
 	"bytes"
-	"strings"
+	"os"
 	"testing"
 
 	"github.com/dave/dst/decorator"
@@ -10,43 +10,20 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-var (
-	testCode = `
-package mypackage
-
-func myfunc() {
-	return 5
-}
-`
-
-	expDot = `
-digraph {
-	File_0_0[label=<File>,shape="box"]
-	File_0_0->Ident_1_0[label="Name",fontsize=12.0]
-	File_0_0->FuncDecl_1_1[label="Decls",fontsize=12.0]
-	Ident_1_0[label=<Ident<br/><font point-size="11.0" face="courier" color="#777777">mypackage</font>>,shape="box"]
-	FuncDecl_1_1[label=<FuncDecl>,shape="box"]
-	FuncDecl_1_1->Ident_2_0[label="Name",fontsize=12.0]
-	FuncDecl_1_1->FieldList_2_1[label="Params",fontsize=12.0]
-	FuncDecl_1_1->BlockStmt_2_2[label="Body",fontsize=12.0]
-	Ident_2_0[label=<Ident<br/><font point-size="11.0" face="courier" color="#777777">myfunc</font>>,shape="box"]
-	FieldList_2_1[label=<FieldList>,shape="box"]
-	BlockStmt_2_2[label=<BlockStmt>,shape="box"]
-	BlockStmt_2_2->ReturnStmt_3_0[label="List",fontsize=12.0]
-	ReturnStmt_3_0[label=<ReturnStmt>,shape="box"]
-	ReturnStmt_3_0->BasicLit_4_0[label="Results",fontsize=12.0]
-	BasicLit_4_0[label=<BasicLit<br/><font point-size="11.0" face="courier" color="#777777">5</font>>,shape="box"]
-}
-`
-)
-
 func TestCreateDot(t *testing.T) {
-	node, err := decorator.Parse(testCode)
+	file, err := os.ReadFile("testdata/sample01.go")
+	require.NoError(t, err)
+
+	node, err := decorator.Parse(file)
 	require.NoError(t, err)
 
 	out := &bytes.Buffer{}
+
 	err = CreateDot(node, out)
 	require.NoError(t, err)
 
-	assert.Equal(t, strings.TrimSpace(expDot), out.String())
+	expected, err := os.ReadFile("testdata/sample01.dot")
+	require.NoError(t, err)
+
+	assert.Equal(t, string(bytes.TrimSpace(expected)), out.String())
 }
