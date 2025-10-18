104443ab6c3148183713645cc355e081b59fbb2b
Fernandez Ludovic <ldez@users.noreply.github.com>
2025-10-18 05:20:36 +0200

    fix: format switch type statement

--- shorten/format.go
+++ shorten/format.go
@@ -145,8 +145,11 @@ func (s *Shortener) formatStmt(stmt dst.Stmt, force bool) {
 	case *dst.SwitchStmt:
 		s.formatStmt(st.Body, false)
 
+	case *dst.TypeSwitchStmt:
+		s.formatStmt(st.Body, false)
+
 	case *dst.BadStmt, *dst.EmptyStmt, *dst.LabeledStmt,
-		*dst.SendStmt, *dst.IncDecStmt, *dst.BranchStmt, *dst.TypeSwitchStmt:
+		*dst.SendStmt, *dst.IncDecStmt, *dst.BranchStmt:
 		// These statements are explicitly defined to improve switch cases exhaustiveness.
 		// They may be handled in the future.
 		if shouldShorten {
